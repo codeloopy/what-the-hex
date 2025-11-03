de830f04f7ee2f66b2f740b4774c79f220581bca
Pedro Monsalve <codeloopy@gmail.com>
2025-11-03 10:06:50 -0500

    Init - Transfer from CodeSandbox

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ColorWindow from "./Components/ColorWindow.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>What the Hex!</h1>
+      <ColorWindow />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/ColorWindow.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import Colors from "../Colors.js";
+
+import "./ColorWindow.css";
+
+export default function ColorWindow() {
+  const [htmlColor, setHtmlColor] = useState("#000000");
+  const [searchColor, setSearchColor] = useState("");
+  const [searchColorResults, setSearchColorResults] = useState([]);
+
+  function handleSelect() {
+    setHtmlColor(event.target.value);
+  }
+
+  function handleSearch() {
+    event.preventDefault();
+    colorResults(searchColor);
+    setSearchColor("");
+  }
+
+  function colorResults(colorQuery) {
+    let results = Colors.filter((color) => {
+      return color.name.toLowerCase().includes(colorQuery);
+    });
+
+    setSearchColorResults(results);
+  }
+
+  return (
+    <div id="colorWindowContainer">
+      <div id="colorWindow" style={{ backgroundColor: htmlColor }}>
+        <span style={{ color: "white" }}>{htmlColor}</span>
+      </div>
+      <select name="colors" id="colors" onChange={handleSelect}>
+        {Colors.map((color) => {
+          return (
+            <option value={color.code} key={color.id}>
+              {color.name}
+            </option>
+          );
+        })}
+      </select>
+      <div id="searchColor">
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Type Color"
+            onChange={(e) => colorResults(setSearchColor(e.target.value))}
+            value={searchColor}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div id="results">
+        {searchColorResults.map((color) => {
+          return (
+            <p style={{ backgroundColor: color.code }} key={color.id}>
+              {color.name} {color.code}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
