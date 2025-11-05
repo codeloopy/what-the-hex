9fb06209d5db155ef8602093081c2ed29ab577d9
Pedro Monsalve <codeloopy@gmail.com>
2025-11-05 16:35:26 -0500

    🐞 ✅

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Nav from "./Components/Nav.jsx";
 import Hero from "./Components/Hero.jsx";
 import ColorWindow from "./Components/ColorWindow.jsx";
-import LearningReact from './Components/LEarningReact.jsx';
+import LearningReact from './Components/LearningReact.jsx';
 import Footer from './Components/Footer.jsx';
 
 function App() {
