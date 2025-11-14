beb9ab85b349cef025d5ef18ab1ece2d2d45d593
Pedro Monsalve <codeloopy@gmail.com>
2025-11-13 22:32:27 -0500

    Add Google Analytics 4 integration and update dependencies 🤞

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {useEffect} from 'react';
+import ReactGA from 'react-ga4';
 import './App.css'
 import Nav from "./Components/Nav.jsx";
 import Hero from "./Components/Hero.jsx";
@@ -6,6 +8,11 @@ import LearningReact from './Components/LearningReact.jsx';
 import Footer from './Components/Footer.jsx';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(import.meta.env.VITE_GA4_MEASUREMENT_ID);
+    ReactGA.send({ hitType: "pageview", page: "/" });
+  }, []);
+
   return (
     <div className='container'>
       <Nav />
